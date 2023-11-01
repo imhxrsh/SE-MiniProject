780f3a446a71f6c61f70f2b4c0529678edbcd8f7
Harsh <itsmehxrsh@gmail.com>
2023-11-01 13:03:35 +0530

    Rename Destinovo to Myriad

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Destinovo</title>
+    <title>Myriad</title>
     <?php include 'includes/style_include.html' ?>
 </head>
 
